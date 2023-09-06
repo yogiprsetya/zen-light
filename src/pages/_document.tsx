288b2b05c308e6e914556a3483176c4b72875c12
Yogi Prasetya <yogiprsetya@gmail.com>
2023-09-06 12:27:27 +0700

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-256x256" />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
